1b990f0f348b696170273ecd25145be02c12208b
XiaoPingJiang <jyx2011719@hotmail.com>
2016-02-21 22:00:53 +0800

    update

--- index.android.js
+++ index.android.js
@@ -11,20 +11,12 @@ import React, {
   View
 } from 'react-native';
 
+import SimpleLifeCycle from './simplelifecycle';
+
 class sw_study_react_native_lifecycle extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <SimpleLifeCycle></SimpleLifeCycle>
     );
   }
 }
